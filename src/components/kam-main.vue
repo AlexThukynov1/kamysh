5bb2fd6b72f4b57a8b8ac1969d91096b9a3b5fb3
Tsykunov Oleksandr <50213824+AlexThukynov1@users.noreply.github.com>
2025-04-10 11:34:09 +0300

    Add files via upload

--- src/components/kam-main.vue
+++ src/components/kam-main.vue
@@ -1,9 +1,22 @@
 <template>
     <main id="main" class="main">
-        <kam-section class="section" id="catalog">
+        <kam-section class="section__slider section" id="catalog">
+            <h1 class="section__title">Catalog</h1>
             <kam-slider/>
+            <kam-button>Call to action</kam-button>
         </kam-section>
-        <kam-section id="price"></kam-section>
+        <kam-section id="price" class="section">
+            <h2 class="section__title">Price</h2>
+            <div class="price__section grid">
+                <div class="price__text-side">
+                    <p class="text-side__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptatem blanditiis fugiat eligendi est aspernatur sit esse, ducimus voluptates at reprehenderit autem praesentium! Est, beatae.</p>
+                    <p class="text-side__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptatem blanditiis fugiat eligendi est aspernatur sit esse, ducimus voluptates at reprehenderit autem praesentium! Est, beatae.</p>
+                    <p class="text-side__text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptatem blanditiis fugiat eligendi est aspernatur sit esse, ducimus voluptates at reprehenderit autem praesentium! Est, beatae.</p>
+                </div>
+                <img class="price__img-side" src="https://placehold.co/600x400" alt="Placeholder image">
+            </div>
+            <kam-button>Call to action</kam-button>
+        </kam-section>
         <kam-section id="description"></kam-section>
         <kam-section id="contacts"></kam-section>
     </main>
@@ -12,10 +25,28 @@
 <script setup lang="ts">
     import kamSection from './layout-components/kam-section.vue';
     import kamSlider from './ui-components/kam-slider.vue';
+    import kamButton from './ui-components/kam-button.vue'
 </script>
 
 <style scoped>
 .main {
     padding-top: var(--header-height);
+}
+.section__slider {
+
+}
+.price__section {
+    grid-template-columns: 1fr 1fr;
+}
+.price__text-side {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+}
+.text-side__text {
+    font-size: var(--normal-font-size);
+}
+.price__img-side {
+
 }
 </style>
---
+++ src/components/ui-components/kam-button.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="button">
+        <span>
+            <slot/>
+        </span>
+    </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.button {
+    padding: 1rem;
+    border-radius: 15px;
+    color: var(--text-color);
+    background-color: var(--first-color) ;
+    transition: border-radius 0.3s, background-color 0.3s, color 0.3s;
+
+}
+.button:hover {
+    background-color: var(--text-color-light) ;
+    border-radius: 5px;
+    color: var(--body-color);
+}
+.button  span {
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+</style>
